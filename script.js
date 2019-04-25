6604dc2ef03adae05fe76ef782f0f0ada64c2b4b
Emmadayan <46944765+Emmadayan@users.noreply.github.com>
2019-04-24 23:22:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,41 +3,4 @@ $( document ).ready(function() {
 
     M.AutoInit();
 
-    $(".friday1").click(function() {
-      $(".friday1p").show();
-    });
-    $(".friday2").click(function() {
-      $(".friday2p").show();
-    });
-    $(".friday3").click(function() {
-      $(".friday3p").show();
-    });
-    $(".friday4").click(function() {
-      $(".friday4p").show();
-    });
-    $(".saturday1").click(function() {
-      $(".saturday1p").show();
-    });
-    $(".saturday2").click(function() {
-      $(".saturday2p").show();
-    });
-    $(".saturday3").click(function() {
-      $(".saturday3p").show();
-    });
-    $(".saturday4").click(function() {
-      $(".saturday4p").show();
-    });
-    $(".saturday5").click(function() {
-      $(".saturday5p").show();
-    });
-    $(".sunday1").click(function() {
-      $(".sunday1p").show();
-    });
-    $(".sunday2").click(function() {
-      $(".sunday2p").show();
-    });
-    $(".sunday3").click(function() {
-      $(".sunday3p").show();
-    });
-
 });
